5f576c0b74ef18ebfe0a838ec2f18db461c66878
FalixRe <Wojtunieczek@o2.pl>
2022-02-17 19:38:00 +0100

    Added localization function in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,111 @@
 
 })(jQuery);
 
+/*
+Template code
+***************************
+*/
+
+
+
+let localizationTable = {
+  en: {
+    intro:'Intro',
+    aboutMe:'About me',
+    gallery:'Gallery',
+    toggleVideo:'Toggle Video',
+    getStarted:'Get started!',
+    browserNotSupportVideo: 'Your browser does not support the video tag.',
+    welcomePage: 'Welcome on my page!',
+    welcomeContent: 'This is a small presentation about myself, done as a project for webdesign course. Scroll down for more information about me, see the photo gallery and find my LinkedIn and Instagram profiles.',
+    findMe: 'Find me',
+    programmer: 'Professional programmer',
+    traveller: 'Travelling fan',
+    hobbist: 'Hobbist',
+    programmerContent: 'I am programming currently in telco buisness. Mostly in C++, but briefly also in Java, lua, ttcn-3. Previously worked with C on AVR platform.',
+    travellerContent: 'During vacations I travel a lot. Especially I like to visit islands, I have been already to most of Canary Islands, Mallorca, Bornholm, Mauritius and also some smaller ones of Croatia and Greece while sailing there.',
+    hobbistContent: 'I got quite a few hobbies. I like learning new things, that is why I ended up as programmer although I have mechatronic degree. It also is one of the reasons I created this page. Currently I am learning Spanish. I also like sailing and almost all kinds of music.',
+    myCareer: 'My career',
+    nokiaTitle: 'NOKIA - Software engineer',
+    nokiaContent: '2018 - Now: Working as a software developer in LTE network development. Recently started part time work as a Scrum Master.',
+    delphiTitle: 'DELPHI (currently APTIV) - Process engineer',
+    delphiContent: '2015-2018: Developing applications and devices to improve production processes',
+    graduateTitle: 'Graduated',
+    graduateContent: '2018: Master\'s degree in mechatronic engineering on University of Science and Technology AGH in Cracow',
+    coursesTitle: 'Courses',
+    coursesContent: 'SAFe practitioner, driver\'s licence, skipper\'s licence',
+    skillsTitle: 'Skills',
+    skillsContent: 'Good teamworking skills, analytical skills, quite fluent with computers. Currently I am most fluent with C++, but I have also brief knowlegde of C, Java, C#, lua, ttcn-3, html, css, js.',
+    languagesTitle: 'Languages',
+    languagesContent: 'Polish, English, Spanish',
+    findMeOn:'Find me on'
+  },
+  pl: {
+    intro:'Wstęp',
+    aboutMe:'O mnie',
+    gallery:'Galeria',
+    toggleVideo:'Wyłącz wideo',
+    getStarted:'Zacznijmy!',
+    browserNotSupportVideo: 'Twoja przeglądarka nie wspiera video w kodzie HTML.',
+    welcomePage: 'Witaj na mojej stronie!',
+    welcomeContent: 'To drobna prezentacja mojej osoby którą zrobiłem w ramach kursu tworzenia stron WWW. Przesuwaj w dół by przeczytać więcej o mnie, zobaczyć galerię zdjęć i znaleźć mnie na LinkedIn lub Instagramie.',
+    findMe: 'Kontakt',
+    programmer: 'Zawodowy programista',
+    traveller: 'Amator podróży',
+    hobbist: 'Zapalony hobbysta',
+    programmerContent: 'Aktualnie programuje w branży telekomunikacyjnej. Głównie w C++, ale mam styczność także z Java, lua, ttcn-3. Wcześniej pracowałem też w C na platformie AVR.',
+    travellerContent: 'Urlopy spędzam głównie podróżując. Szególnie lubie odwiedzać wyspy, byłem już na większości z Wysp Kanaryjskich, Majorce, Bornholmie, Mauritiusie i paru innych żeglując na Chorwacji i w Grecji.',
+    hobbistContent: 'Mam całkiem szerokie zainteresownia. Lubię uczyć się nowych rzeczy, dlatego skończyłem jako programista pomimo iż ukonczyłem mechatronikę. To także jeden z powodów przez które stworzyłem tą stron. Aktualnie uczę się hiszpańskiego. Lubię także żeglować i prawie każdy rodzaj muzyki.',
+    myCareer: 'Moja kariera',
+    nokiaTitle: 'NOKIA - Inżynier oprogramowania',
+    nokiaContent: '2018 - Teraz: Pracuje jako programista rozwijając i utrzymując sieć LTE. Niedawno zaczałem także cześciowo pełnić obowiązki Scrum Mastera.',
+    delphiTitle: 'DELPHI (aktualnie APTIV) - Inżynier procesu',
+    delphiContent: '2015-2018: Rozwijałem oprogramowanie i urządzenia w celu ulepszania procesu produkcji',
+    graduateTitle: 'Dyplom',
+    graduateContent: '2018: Dyplom magistra inżynierii mechatronicznej na Akademi Górniczo-Hutniczej w Krakowie',
+    coursesTitle: 'Kursy',
+    coursesContent: 'SAFe practitioner, prawo jazdy, patent żeglarski',
+    skillsTitle: 'Umiejętności',
+    skillsContent: 'Dobre umiejętności pracy w zespole, umiejętności analityczne, dobrze radzę sobie z komputerami. Aktulanie najlepiej radzę sobie z C++, ale znam też podstawy C, Java, C#, lua, ttcn-3, html, css, js.',
+    languagesTitle: 'Języki',
+    languagesContent: 'Polski, Angielski, Hiszpański',
+    findMeOn:'Znajdź mnie na'
+  },
+  es: {
+    intro:'Intro',
+    aboutMe:'Sobre mí',
+    gallery:'Galería',
+    toggleVideo:'Desactivar Video',
+    getStarted:'Comencemos!',
+    browserNotSupportVideo: 'Tu navegador no soporta HTML video.',
+    welcomePage: 'Bienvenidos a mi página!',
+    welcomeContent: 'Esta es una pequeña presentación sobre mí, hecha como proyecto para un curso de diseño web. Desplácese hacia abajo para obtener más información sobre mí, ver la galería de fotos y mis perfiles de LinkedIn e Instagram.',
+    findMe: ' Encontrarme',
+    programmer: 'Un programador profesional',
+    traveller: 'Un viajero',
+    hobbist: 'Un aficionado',
+    programmerContent: 'Ahora estoy programando en la empresa de telecomunicaciones. Principalmente en C++, pero un poco también en Java, lua, ttcn-3. Anterior trabajé con C en la plataforma AVR también.',
+    travellerContent: 'Viajo mucho en mis vacaciones. Me gusta mucho visitar las islas, Estuve en la mayoria de Islas Canarias, Mallorca, Bornholm, Mauricio y, durante los cruceros, algunas pequeñas islas de Croacia y Grecia.',
+    hobbistContent: 'Tengo algunas pasatiempos. Me gusta aprender cosas nuevas, por eso soy programador aunque me he graduado en mecatrónica. Por eso, también, creé esta página. Actualmente, aprendo Español en nivel B1. Además, me gusta mucho navegar en barco de vela y escuchar casi todo tipo de música.',
+    myCareer: 'Mi carrera ',
+    nokiaTitle: 'NOKIA - Ingeniero de software',
+    nokiaContent: '2018 - Presente: Estoy trabajando como ingeniero de software en desarollo de LTE red. Acabo de empazar a trabajar en parte como ScrumMaster.',
+    delphiTitle: 'DELPHI (Ahora es APTIV) - Ingeniero de procesos.',
+    delphiContent: '2015-2018: Desarrollaba de aplicaciones y dispositivos para mejorar los procesos de producción',
+    graduateTitle: 'Carrera',
+    graduateContent: '2018: Maestría en Ingeniería Mecatrónica en la Universidad de Ciencia y Tecnología AGH en Cracovia',
+    coursesTitle: 'Cursos',
+    coursesContent: 'SAFe practitioner, licencia de conducir, licencia de navegación(velero)',
+    skillsTitle: 'Habilidades',
+    skillsContent: 'Buenas habilidades de trabajo en equipo, habilidades analíticas, bastante fluidez con las ordenadoras. Actualmente soy más fluido con C++, pero también tengo uns conocimiento de C, Java, C#, lua, ttcn-3, html, css, js.',
+    languagesTitle: 'Lenguajes',
+    languagesContent: 'Polaco, Inglese, Español',
+    findMeOn:'Encuéntrame en '
+    
+  }
+}
+
+
 window.onload = function() {
     let myQuotes = ['Hi! Welcome, hello!',
                     'Let\'s sail!',
@@ -48,3 +153,14 @@ window.onload = function() {
                     ];
     document.getElementById('my-quote').innerHTML = '"'+ myQuotes[Math.floor(myQuotes.length * Math.random())]+'"';  
 };
+
+
+function localizeWord(word) {
+  return (localizationTable[document.getElementById('language').value][word]);
+}
+
+function localizeAll() {
+  for (element of (document.querySelectorAll('[data-localizationName]'))) {
+    element.innerHTML = localizeWord(element.getAttribute('data-localizationName'));
+  }
+}
